2a02a988e818e3963644ef983a793173fae1a516
Dash-5-stone <josephhounkpe852@gmail.com>
2026-01-12 11:30:59 +0100

    modification

--- Bibliotheque.cpp
+++ Bibliotheque.cpp
@@ -123,9 +123,8 @@ void Bibliotheque::AfficherClassementLecteurs() {
     std::vector<Lecteur> listeTriee = lecteurs_;
     int n = listeTriee.size();
 
-
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n -1; j++) {
             if (listeTriee[j].getIsbnEmprunter().size() < listeTriee[j + 1].getIsbnEmprunter().size()) {
                
                 Lecteur tmp = listeTriee[j];
